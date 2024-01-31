5d20e74471b0a4ded12f7a9b31588162011aeb94
Ryan Dey <techrpd15113@gmail.com>
2024-01-31 13:02:33 -0600

    Setup credentials/session provider

---
+++ app/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Crentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "email@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        const res = await fetch("/your/endpoint", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        })
+
+        const user = await res.json()
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user
+        }
+        // Return null if user data could not be retrieved
+        return null
+      },
+    }),
+  ],
+}
+
+export const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,16 @@
 "use client"
 
+import { Session, getServerSession } from "next-auth"
+import { SessionProvider } from "next-auth/react"
 import { ChakraProvider } from "@chakra-ui/react"
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+export function Providers(
+  { session }: { session: Session },
+  { children }: { children: React.ReactNode }
+) {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider>{children}</ChakraProvider>
+    </SessionProvider>
+  )
 }
